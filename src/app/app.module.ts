ea4590a2f1893c6a2c7f88f6c8c5ed3628a1af07
F1FB <F1FB@eurofins.com>
2022-10-01 18:50:12 +0200

    Project scaffolding + basic unit tests green

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StockAddComponent } from './stock-tracker/stock-add/stock-add.component';
 import { StockTrackerComponent } from './stock-tracker/stock-tracker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StockTrackerComponent
+    StockTrackerComponent,
+    StockAddComponent
   ],
   imports: [
     BrowserModule,
--- src/app/stock-tracker/stock-tracker.component.ts
+++ src/app/stock-tracker/stock-tracker.component.ts
@@ -1,48 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { StockTrackerStorageService } from './stock-tracker-storage.service';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-stock-tracker',
-  template: `
-<div class="container">
-  <form #trackerForm="ngForm">
-    <div class="form-group">
-      <div>Enter the symbol of a stock to track (i.e. AAPL, TSLA, GOOGL)</div>
-      <span>
-        <input 
-          required
-          type="text" 
-          class="form-control" 
-          id="stockInput"
-          name="stockInput"
-          [(ngModel)]="stockInput"></span>
-      <span>
-        <button 
-          type="button" 
-          class="btn btn-default"
-          id="trackBtn"
-          (click)="trackStock()"
-          [disabled]="!trackerForm.form.valid">Track Stock</button>
-      </span>
-    </div>
-  </form>  
-</div>
-`,
+  template: `<app-stock-add></app-stock-add>`,
 })
-export class StockTrackerComponent implements OnInit {
-
-  stockInput: string = '';
-
-  constructor(private storageService: StockTrackerStorageService) { }
-
-  ngOnInit(): void {
-  }
-
-  trackStock() : void {
-    this.storageService.saveSymbol(this.stockInput);
-
-    console.log(this.storageService.getAllSymbols());
-    
-  }
-
+export class StockTrackerComponent {
 }
