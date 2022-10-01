d64e1610f6986def3c20ed864f51aae954533145
F1FB <F1FB@eurofins.com>
2022-10-01 16:15:06 +0200

    Add stock-tracker component, routing + bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StockSearchComponent } from './stock-tracker/stock-tracker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StockSearchComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
