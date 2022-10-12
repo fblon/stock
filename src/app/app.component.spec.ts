a87098c2ebd926efa6ec02e814c20567f80470a2
F1FB <F1FB@eurofins.com>
2022-10-12 17:28:18 +0200

    Add eslint-plugin-jasmine and linting for jasmine

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,12 +17,14 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it('should have as title "Stock Tracking App"', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('Stock Tracking App');
   });
 });
