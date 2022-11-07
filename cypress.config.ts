9e08c4e7481dbe1ce7c5a46102f44c99aa9d0bf3
F1FB <F1FB@eurofins.com>
2022-11-07 18:12:28 +0100

    Remove all.cy.ts execution from cypress run and remove *.mp4 from source control

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
   },
   e2e: {
     baseUrl: 'http://localhost:4200/',
-    specPattern: "cypress/tests/**/*.cy.ts"
+    specPattern: "cypress/tests/**/*.cy.ts",
+    setupNodeEvents(on, config) {
+      if (config.isTextTerminal) {
+        // skip the all.cy.js spec in "cypress run" mode
+        return {
+          excludeSpecPattern: ['cypress/tests/all.cy.ts'],
+        }
+      }
+    },    
   },
 });
