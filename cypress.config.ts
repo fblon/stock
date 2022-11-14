04ccc374d7d625445d4177ebb194a26d0fd01201
F1FB <F1FB@eurofins.com>
2022-11-14 18:19:40 +0100

    Enable eslint no-non-null-assertion + increase cypress request timeout to fix first test execution failure

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200/',
     specPattern: "cypress/tests/**/*.cy.ts",
-    requestTimeout: 15000, // first test is slow in run mode, don't know why :(
+    requestTimeout: 200000, // first test is slow in run mode, don't know why :(
     setupNodeEvents(on, config) {
       if (config.isTextTerminal) {
         // skip the all.cy.js spec in "cypress run" mode
